6f90a2bb47ba40bc2cd21135563a1ada94eb4b54
Gennady Terekhov <gterechov@team.amocrm.com>
2020-09-17 19:39:47 +0300

    completed task 1

--- 1/main.py
+++ 1/main.py
@@ -1,4 +1,6 @@
 import math
+import sys
+
 
 # get digital representation of letter coordinate
 def get_digit(ch):
@@ -10,7 +12,9 @@ def get_digit(ch):
 
 # get distance between 2 points
 def get_distance(point1, point2):
-    pass
+    a = abs(point1['x'] - point2['x'])
+    b = abs(point1['y'] - point2['y'])
+    return hypothenuse(a, b)
 
 
 # calculate hypothenuse
@@ -18,41 +22,34 @@ def hypothenuse(a, b):
     return math.sqrt(a**2 + b**2)
 
 
-# get all possible fields for a knight
-def get_possible_fields(field)
-    fields = []
-    for i in range(1, 9):
-        temp_x = field.x + 1
-        temp_y = field.y + 2
-    return [field, field]
+# get field dict from str
+def get_field(s):
+    x1 = int(get_digit(s[0]))
+    y1 = int(s[1])
+    return {"x": x1, "y": y1}
 
 
-
-# get fields: current field and field to check
-field1_str = input("field 1")
-field2 = input("field 2")
-
-# set coordinates for field 1
-x1 = get_digit(field1_str[0])
-y1 = field1_str[1]
-
-# set coordinates for field 2
-x2 = get_digit(field2_str[0])
-y2 = field2_str[1]
+if (len(sys.argv) != 3):
+    print("invalid number of arguments")
+    sys.exit()
 
 
-field1 = {
-    "x": x1,
-    "y": y1
-}
+# get fields: current field and field to check from input as strings
+# field1_str = input("enter field 1>")
+# field2_str = input("enter field 2>")
+field1_str = sys.argv[1]
+field2_str = sys.argv[2]
 
+# convert from field str to point a1 to x:1 y:1
+field1 = get_field(field1_str)
+field2 = get_field(field2_str)
 
-field2 = {
-    "x": x2,
-    "y": y2
-}
+# sampe fields
+a1 = get_field("a1")
+b3 = get_field("b3")
 
-
-result = (field2 in get_possible_fields(field1))
+# distance possible for knight to go
+knight_distance = get_distance(a1, b3)
+current_dictance = get_distance(field1, field2)
+result = (current_dictance == knight_distance)
 print(result)
-
