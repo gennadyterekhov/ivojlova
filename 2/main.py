6caf0f9b5459dfa814e93a3dd420fdb6377b7c4d
Gennady Terekhov <ter.ge@yandex.ru>
2020-09-19 17:19:56 +0300

    2 ok again

--- 2/main.py
+++ 2/main.py
@@ -1,11 +1,14 @@
 import math
 import sys
+import time
 
+
+# https://stackoverflow.com/questions/18172257/efficient-calculation-of-fibonacci-series
 # get n'th fibonacci number recursively
-def fib(n):
-    if (n < 2):
-        return n
-    return fib(n - 1) + fib(n - 2)
+def fib(n, fib_dict = {0: 0, 1: 1}):
+    if n not in fib_dict:
+        fib_dict[n] = fib(n - 1, fib_dict) + fib(n - 2, fib_dict)
+    return fib_dict[n]
 
 
 if (len(sys.argv) != 2):
@@ -16,8 +19,14 @@ if (len(sys.argv) != 2):
 # get n from args
 n = int(sys.argv[1])
 
+
+# start timer
+start_time = time.time()
+
 # result is n'th fibonacci number
 res = fib(n)
 print(res)
 
 
+# print execution time
+print("\nexecution time {} seconds".format(time.time() - start_time))
